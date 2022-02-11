0cb86170525cf50e2dd10c9c2e46043ef1633802
basedx32 <95654307+basedx32@users.noreply.github.com>
2022-02-10 22:46:33 -0700

    Update mover.go

--- mover/mover.go
+++ mover/mover.go
@@ -3,3 +3,21 @@ package main
 import (
   "fmt"
 )
+
+func main() {
+	currentDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatalf(err)
+	}
+	iterate(currentDirectory)
+}
+
+func iterate(path string) {
+	filepath.Walk(path, func(path string, info os.FileInfo, err error ) error {
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		fmt.Printf("File name: %s\n", info.Name())
+		return nil
+	})
+}
